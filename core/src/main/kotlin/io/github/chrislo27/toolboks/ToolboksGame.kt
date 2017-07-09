4bff171bb7c39886b1b802224f276e98e51e0489
chrislo27 <kinchrislo@gmail.com>
2017-07-09 10:35:11 -0700

    Tweak logger order

--- core/src/main/kotlin/io/github/chrislo27/toolboks/ToolboksGame.kt
+++ core/src/main/kotlin/io/github/chrislo27/toolboks/ToolboksGame.kt
@@ -3,21 +3,35 @@ package io.github.chrislo27.toolboks
 import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.OrthographicCamera
+import io.github.chrislo27.toolboks.logging.Logger
+import io.github.chrislo27.toolboks.logging.SysOutPiper
 import io.github.chrislo27.toolboks.registry.AssetRegistry
 import io.github.chrislo27.toolboks.registry.ScreenRegistry
 import io.github.chrislo27.toolboks.tick.TickController
 import io.github.chrislo27.toolboks.tick.TickHandler
 import io.github.chrislo27.toolboks.version.Version
 
-public abstract class ToolboksGame(val version: Version,
-                                   val emulatedSize: Pair<Int, Int>, val lockToEmulatedSize: Boolean) : Game(), TickHandler {
+public abstract class ToolboksGame(val logger: Logger, val logToFile: Boolean,
+                                   val version: Version,
+                                   val emulatedSize: Pair<Int, Int>, val lockToEmulatedSize: Boolean)
+    : Game(), TickHandler {
 
     lateinit var originalResolution: Pair<Int, Int>
         private set
     val defaultCamera: OrthographicCamera = OrthographicCamera()
     val tickController: TickController = TickController()
 
+    /**
+     * Should include the version
+     */
+    abstract fun getTitle(): String
+
     override fun create() {
+        if (logToFile) {
+            SysOutPiper.pipe(this)
+        }
+        Toolboks.LOGGER = logger
+
         originalResolution = Pair(Gdx.graphics.width, Gdx.graphics.height)
         resetCamera()
         tickController.init(this)
@@ -27,6 +41,8 @@ public abstract class ToolboksGame(val version: Version,
         defaultCamera.update()
         tickController.update()
         super.render()
+
+
     }
 
     override fun tickUpdate(tickController: TickController) {
--- desktop/src/main/kotlin/io/github/chrislo27/toolboks/desktop/ToolboksDesktopLauncher.kt
+++ desktop/src/main/kotlin/io/github/chrislo27/toolboks/desktop/ToolboksDesktopLauncher.kt
@@ -2,25 +2,15 @@ package io.github.chrislo27.toolboks.desktop
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
-import io.github.chrislo27.toolboks.Toolboks
 import io.github.chrislo27.toolboks.ToolboksGame
-import io.github.chrislo27.toolboks.logging.Logger
-import io.github.chrislo27.toolboks.logging.SysOutPiper
 
 
 class ToolboksDesktopLauncher(val game: ToolboksGame) {
 
     val config = LwjglApplicationConfiguration()
-    var logToFile: Boolean = true
-    var logger: Logger? = null
 
     init {
         System.setProperty("file.encoding", "UTF-8")
-    }
-
-    fun disableLoggingToFile(): ToolboksDesktopLauncher {
-        logToFile = false
-        return this
     }
 
     inline fun editConfig(func: LwjglApplicationConfiguration.() -> Unit): ToolboksDesktopLauncher {
@@ -28,22 +18,10 @@ class ToolboksDesktopLauncher(val game: ToolboksGame) {
         return this
     }
 
-    fun setLogger(logger: Logger?): ToolboksDesktopLauncher {
-        this.logger = logger
-        return this
-    }
-
     fun launch(): LwjglApplication {
-        val logger: Logger = logger ?: Logger()
-
-        return object : LwjglApplication(game, config) {
-            init {
-                Toolboks.LOGGER = logger
-                if (logToFile) {
-                    SysOutPiper.pipe(game)
-                }
-            }
+        val app = object : LwjglApplication(game, config) {
         }
+        return app
     }
 
 }
