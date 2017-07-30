6e2cd43fb30be25bb6b0422dcc0126aa31b117a1
chrislo27 <kinchrislo@gmail.com>
2017-07-30 13:47:27 -0700

    Add batchCall

--- core/src/main/kotlin/io/github/chrislo27/toolboks/util/gdxutils/SpriteBatchUtils.kt
+++ core/src/main/kotlin/io/github/chrislo27/toolboks/util/gdxutils/SpriteBatchUtils.kt
@@ -1,6 +1,7 @@
 package io.github.chrislo27.toolboks.util.gdxutils
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Matrix4
 import io.github.chrislo27.toolboks.ToolboksGame
 
 fun SpriteBatch.fillRect(x: Float, y: Float, width: Float, height: Float) {
@@ -12,4 +13,16 @@ fun SpriteBatch.drawRect(x: Float, y: Float, width: Float, height: Float, line: 
     this.draw(ToolboksGame.smallTexture, x, y + height, width, -line)
     this.draw(ToolboksGame.smallTexture, x, y, line, height)
     this.draw(ToolboksGame.smallTexture, x + width, y, -line, height)
+}
+
+inline fun SpriteBatch.batchCall(projection: Matrix4 = this.projectionMatrix, drawFunction: SpriteBatch.() -> Unit) {
+    val oldProjection = this.projectionMatrix
+    val oldColor = this.packedColor
+
+    this.begin()
+    this.drawFunction()
+    this.end()
+
+    this.projectionMatrix = oldProjection
+    this.setColor(oldColor)
 }
