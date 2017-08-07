62947c160fa1b91ee124afe522c04394643639dd
chrislo27 <kinchrislo@gmail.com>
2017-08-06 17:11:11 -0700

    i can't even

--- core/src/main/kotlin/io/github/chrislo27/toolboks/ui/ContextMenu.kt
+++ core/src/main/kotlin/io/github/chrislo27/toolboks/ui/ContextMenu.kt
@@ -10,16 +10,6 @@ open class ContextMenu<S : ToolboksScreen<*, *>> : Stage<S> {
 
     constructor(parent: UIElement<S>) : super(parent.stage, parent.stage.camera)
 
-    override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
-        val result = super.touchUp(screenX, screenY, pointer, button)
-
-        if (!result) {
-            this.visible = false
-        }
-
-        return result
-    }
-
     override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
         val result = super.touchDown(screenX, screenY, pointer, button)
 
