8c0d088f4d5816b63f583e4609f75528d84255dd
chrislo27 <kinchrislo@gmail.com>
2017-08-12 22:30:05 -0700

    Add Rectangle overloads

--- core/src/main/kotlin/io/github/chrislo27/toolboks/util/gdxutils/SpriteBatchUtils.kt
+++ core/src/main/kotlin/io/github/chrislo27/toolboks/util/gdxutils/SpriteBatchUtils.kt
@@ -2,10 +2,15 @@ package io.github.chrislo27.toolboks.util.gdxutils
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Matrix4
+import com.badlogic.gdx.math.Rectangle
 import io.github.chrislo27.toolboks.ToolboksGame
 
 fun SpriteBatch.fillRect(x: Float, y: Float, width: Float, height: Float) {
     this.draw(ToolboksGame.smallTexture, x, y, width, height)
+}
+
+fun SpriteBatch.fillRect(rect: Rectangle) {
+    this.fillRect(rect.x, rect.y, rect.width, rect.height)
 }
 
 fun SpriteBatch.drawRect(x: Float, y: Float, width: Float, height: Float, lineX: Float, lineY: Float) {
@@ -17,6 +22,14 @@ fun SpriteBatch.drawRect(x: Float, y: Float, width: Float, height: Float, lineX:
 
 fun SpriteBatch.drawRect(x: Float, y: Float, width: Float, height: Float, line: Float) {
     this.drawRect(x, y, width, height, line, line)
+}
+
+fun SpriteBatch.drawRect(rect: Rectangle, lineX: Float, lineY: Float) {
+    this.drawRect(rect.x, rect.y, rect.width, rect.height, lineX, lineY)
+}
+
+fun SpriteBatch.drawRect(rect: Rectangle, line: Float) {
+    this.drawRect(rect, line, line)
 }
 
 inline fun SpriteBatch.batchCall(projection: Matrix4 = this.projectionMatrix, drawFunction: SpriteBatch.() -> Unit) {
