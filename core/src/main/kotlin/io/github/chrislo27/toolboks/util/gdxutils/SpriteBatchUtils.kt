1fa4236f1f36968264ebd6a943624be73b545ead
chrislo27 <kinchrislo@gmail.com>
2017-09-10 12:25:37 -0700

    Tweak drawRect

--- core/src/main/kotlin/io/github/chrislo27/toolboks/util/gdxutils/SpriteBatchUtils.kt
+++ core/src/main/kotlin/io/github/chrislo27/toolboks/util/gdxutils/SpriteBatchUtils.kt
@@ -16,8 +16,8 @@ fun SpriteBatch.fillRect(rect: Rectangle) {
 fun SpriteBatch.drawRect(x: Float, y: Float, width: Float, height: Float, lineX: Float, lineY: Float) {
     this.draw(ToolboksGame.smallTexture, x, y, width, lineY)
     this.draw(ToolboksGame.smallTexture, x, y + height, width, -lineY)
-    this.draw(ToolboksGame.smallTexture, x, y, lineX, height)
-    this.draw(ToolboksGame.smallTexture, x + width, y, -lineX, height)
+    this.draw(ToolboksGame.smallTexture, x, y + lineY, lineX, height - lineY * 2)
+    this.draw(ToolboksGame.smallTexture, x + width, y + lineY, -lineX, height - lineY * 2)
 }
 
 fun SpriteBatch.drawRect(x: Float, y: Float, width: Float, height: Float, line: Float) {
