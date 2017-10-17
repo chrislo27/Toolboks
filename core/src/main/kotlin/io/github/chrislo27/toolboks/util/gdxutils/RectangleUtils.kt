f86785b213367ddd93e1c8e90295f26077759169
chrislo27 <kinchrislo@gmail.com>
2017-10-16 20:44:57 -0700

    Fix RectangleUtils to use properties instead of functions

--- core/src/main/kotlin/io/github/chrislo27/toolboks/util/gdxutils/RectangleUtils.kt
+++ core/src/main/kotlin/io/github/chrislo27/toolboks/util/gdxutils/RectangleUtils.kt
@@ -3,6 +3,6 @@ package io.github.chrislo27.toolboks.util.gdxutils
 import com.badlogic.gdx.math.Rectangle
 
 
-fun Rectangle.getMaxX(): Float = this.x + this.width
+val Rectangle.maxX: Float get() = this.x + this.width
 
-fun Rectangle.getMaxY(): Float = this.y + this.height
+val Rectangle.maxY: Float get() = this.y + this.height
