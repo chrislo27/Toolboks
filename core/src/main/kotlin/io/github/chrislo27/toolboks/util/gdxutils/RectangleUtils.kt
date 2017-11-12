3af59aa8ef1026116e89af40765f57c2bc9b2cef
chrislo27 <kinchrislo@gmail.com>
2017-11-12 15:37:43 -0800

    Fix intersects again

--- core/src/main/kotlin/io/github/chrislo27/toolboks/util/gdxutils/RectangleUtils.kt
+++ core/src/main/kotlin/io/github/chrislo27/toolboks/util/gdxutils/RectangleUtils.kt
@@ -10,5 +10,5 @@ val Rectangle.maxY: Float get() = this.y + this.height
  * Same as [Rectangle.overlaps] but has equality instead of just less/greater than comparisons.
  */
 fun Rectangle.intersects(r: Rectangle): Boolean {
-    return x <= r.x + r.width && x + width >= r.x && y <= r.y + r.height && y + height >= r.y
+    return this.overlaps(r) || (x == r.x && y == r.y && maxX == r.maxX && maxY == r.maxY)
 }
