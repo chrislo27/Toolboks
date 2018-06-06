c65e64f4597b29409c85587ff4f83380dd1ac24f
chrislo27 <kinchrislo@gmail.com>
2018-06-06 15:28:31 -0700

    Add basic transition support

---
+++ core/src/main/kotlin/io/github/chrislo27/toolboks/transition/TransitionScreen.kt
@@ -0,0 +1,97 @@
+package io.github.chrislo27.toolboks.transition
+
+import com.badlogic.gdx.Screen
+import io.github.chrislo27.toolboks.ToolboksGame
+import io.github.chrislo27.toolboks.ToolboksScreen
+import kotlin.math.absoluteValue
+
+
+/**
+ * A one-use transition screen.
+ *
+ * During a transition, only the render method is called for the entry and destination screens.
+ *
+ */
+class TransitionScreen<G : ToolboksGame>(main: G,
+                                         val entryScreen: Screen?, val destScreen: Screen?,
+                                         entryTransition: Transition?, destTransition: Transition?)
+    : ToolboksScreen<G, TransitionScreen<G>>(main) {
+
+    val entryTransition: Transition = entryTransition ?: Transition.EMPTY
+    val destTransition: Transition = destTransition ?: Transition.EMPTY
+
+    init {
+        if (this.entryTransition.duration < 0f)
+            throw IllegalArgumentException("Duration of entry transition is negative: ${this.entryTransition.duration}")
+        if (this.destTransition.duration < 0f)
+            throw IllegalArgumentException("Duration of dest transition is negative: ${this.destTransition.duration}")
+    }
+
+    val duration: Float = (this.entryTransition.duration + this.destTransition.duration).absoluteValue
+    var timeElapsed: Float = 0f
+        private set
+    /**
+     * The total percentage from 0.0 to 1.0 of the transition state.
+     */
+    val percentageTotal: Float
+        get() = if (duration == 0f) 1f else (timeElapsed / duration).coerceIn(0f, 1f)
+    /**
+     * The total percentage from 0.0 to 1.0 of the ENTRY transition.
+     */
+    val percentageEntry: Float
+        get() = if (this.entryTransition.duration == 0f) {
+            1f
+        } else {
+            (timeElapsed / this.entryTransition.duration).coerceIn(0f, 1f)
+        }
+    /**
+     * The total percentage from 0.0 to 1.0 of the DESTINATION transition.
+     */
+    val percentageDest: Float
+        get() = if (this.destTransition.duration == 0f) {
+            1f
+        } else {
+            ((timeElapsed - this.entryTransition.duration) / this.destTransition.duration).coerceIn(0f, 1f)
+        }
+    val done: Boolean
+        get() = percentageTotal >= 1.0f
+    val doneEntry: Boolean
+        get() = percentageEntry >= 1.0f
+
+    override fun render(delta: Float) {
+        super.render(delta)
+        timeElapsed += delta
+
+        // Render screen
+        (if (doneEntry) destScreen else entryScreen)?.render(delta)
+        // Render transition
+        val transition = (if (doneEntry) destTransition else entryTransition)
+        transition.render(this)
+
+        if (transition.overrideDone) {
+            timeElapsed = if (doneEntry) {
+                duration
+            } else {
+                entryTransition.duration
+            }
+        }
+
+        if (done) {
+            dispose()
+            main.screen = destScreen
+        }
+    }
+
+    override fun getDebugString(): String? {
+        return super.getDebugString()
+    }
+
+    override fun tickUpdate() {
+    }
+
+    override fun dispose() {
+        entryTransition.dispose()
+        destTransition.dispose()
+    }
+
+}
